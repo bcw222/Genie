67688b99fd7c5a7e08121e4f74add3d553337c9b
peter-bear <2388152240@qq.com>
2025-08-30 14:40:27 -0400

    remove redundant codes and rename the test file

--- Docker/test_docker_deploy.py
+++ Docker/test_docker_deploy.py
@@ -44,24 +44,8 @@ Function: Clear the loaded reference audio cache on the server.
 Request Parameters: None.
 """
 
-import os
-
-# (Optional) We recommend manually specifying the Hubert path for Genie.
-# Download from Huggingface: https://huggingface.co/High-Logic/Genie
-# Note: If this line is not set, Genie will automatically download the model from Huggingface.
-os.environ['HUBERT_MODEL_PATH'] = r"C:\path\to\chinese-hubert-base.onnx"
-
-# (Optional) We recommend manually specifying the dictionary path for pyopenjtalk.
-# Download from Huggingface: https://huggingface.co/High-Logic/Genie
-# Note: If this line is not set, pyopenjtalk will automatically download the dictionary.
-os.environ['OPEN_JTALK_DICT_DIR'] = r"C:\path\to\open_jtalk_dic_utf_8-1.11"
-
-import time
 import requests
 import pyaudio
-import multiprocessing
-
-import genie_tts as genie
 
 # --- Configuration ---
 # Server address
